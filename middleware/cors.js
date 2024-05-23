b40bfcb05a8cc4e1e6bee8b781274e52e3b5ba62
ARamos <albertramos902@gmail.com>
2024-05-23 14:26:24 +0100

    refractoring code to separate routes

---
+++ middleware/cors.js
@@ -0,0 +1,25 @@
+import cors from "cors"
+
+const ACCEPTED_ORIGINS = [
+  "http://localhost:8080",
+  "http://localhost:3000",
+  "http://localhost:1234",
+  "https://ourmoviesweb.com",
+]
+
+
+export const corsMiddleware = (acceptedOrigins = ACCEPTED_ORIGINS) => cors({
+  origin: (origin, callback) => {
+    
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error("Cors not allowed"))
+  }
+
+})
--- routes/movies.js
+++ routes/movies.js
@@ -1,16 +1,43 @@
 import { Router } from "express";
+import { randomUUID } from "crypto";
+import express, { json } from "express";
+import fs from "node:fs";
+
+const app = express();
+app.use(json());
+
+app.disable("x-powered-by");
+
+
+app.use(json());
+
+
+const ACCEPTED_ORIGINS = [
+  "http://localhost:8080",
+  "http://localhost:3000",
+  "http://localhost:1234",
+  //and production urls:
+  "https://ourmoviesweb.com",
+  //and any other website that we want to allow, we just need to get the origin from the request header
+];
+
+import {validateMovie, validatePartialMovie} from "../schemas/movieScheme"
 
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
-const movies = require("./movies.json");
+const movies = require(".movies.json");
 
-const router = Router()
+const origin = req.header("origin");
+if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  res.header("Access-Control-Allow-Origin", origin);
+}
 
-router.get('/', (req, res) => {
-  
-  const origin = req.header('origin')
+export const moviesRouter = Router();
+
+moviesRouter.get("/", (req, res) => {
+  const origin = req.header("origin");
   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header("Access-Control-Allow-Origin", origin)
+    res.header("Access-Control-Allow-Origin", origin);
   }
 
   const { genre } = req.query;
@@ -21,15 +48,69 @@ router.get('/', (req, res) => {
     return res.json(filteredMovies);
   }
   res.json(movies);
-})
+});
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const movie = movies.find((movie) => movie.id === id);
+moviesRouter.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
 
-    if (movie) {
-      res.json(movie);
-    } else {
-      res.status(404).json({ error: "Movie not found" });
-    }
-})
+  if (movie) {
+    res.json(movie);
+  } else {
+    res.status(404).json({ error: "Movie not found" });
+  }
+});
+
+moviesRouter.post("/", (req, res) => {
+  const result = validateMovie(req.body);
+
+  if (result.error) {
+    return res.status(400).json({
+      error: JSON.parse(result.error.message),
+    });
+  }
+
+  const newMovie = {
+    id: randomUUID(),
+    ...result.data,
+  };
+  movies.push(newMovie);
+  res.status(201).json(newMovie);
+});
+
+
+moviesRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.find((movie) => movie.id === id);
+  if (movieIndex === -1) {
+    return res.status(400).json({ message: "Movie not found" });
+  }
+  movies.splice(movieIndex, 1);
+  return res.json({ message: "Movie deleted" });
+});
+
+
+moviesRouter.patch("/:id", (req, res) => {
+  const result = validatePartialMovie(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const { id } = req.params;
+
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (!movieIndex === -1)
+    return res.status(404).json({ message: "Movie not found" });
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  };
+
+  movies[movieIndex] = updateMovie;
+
+  return res.json(updateMovie);
+});
+
